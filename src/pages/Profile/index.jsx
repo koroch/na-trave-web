d63249b881835baf48d0013edfc8e34626caeaaf
Jonatan Koroch de Aguiar <jonatanaguiar1998@hotmail.com>
2023-02-22 10:27:27 -0300

    fix: Corrected score

--- src/pages/Profile/index.jsx
+++ src/pages/Profile/index.jsx
@@ -96,8 +96,8 @@ export const Profile = () => {
                                 homeTeam={game.homeTeam}
                                 awayTeam={game.awayTeam}
                                 gameTime={format(new Date(game.gameTime), 'H:mm')}
-                                homeTeamScore={user?.hunches?.[game.id]?.homeTeamScore || ""}
-                                awayTeamScore={user?.hunches?.[game.id]?.awayTeamScore || ""}
+                                homeTeamScore={user?.hunches?.[game.id]?.homeTeamScore}
+                                awayTeamScore={user?.hunches?.[game.id]?.awayTeamScore}
                                 disabled={true}
                             />     
                         ))}
